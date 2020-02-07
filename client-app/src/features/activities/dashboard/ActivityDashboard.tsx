d9224d41e376daef7a6289484e320d7da49db699
Redo <redar_chicho@hotmail.com>
2020-02-06 23:21:45 -0600

    added route

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,45 +1,21 @@
-import React, { SyntheticEvent, useContext } from "react";
+import React, {useContext } from "react";
 import { Grid } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../detials/ActivityDetails";
 import ActivityForm from "./form/form/ActivityForm";
 import { observer } from "mobx-react-lite";
 import ActivityStore from '../../../app/stores/activityStore'
-interface IProps {
-  activities: IActivity[];
-  setEditMode: (editMode: boolean) => void;
-  setSelectedActivity: (activity: IActivity | null) => void;
-  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-  submitting: boolean;
-  target: string;
-}
-const ActivityDashboard: React.FC<IProps> = ({
 
-  deleteActivity,
-  submitting,
-  target
-}) => {
+const ActivityDashboard: React.FC = () => {
   const activityStore = useContext(ActivityStore)
-  const {editMode, selectedActivity,  } = activityStore;
+  const {editMode, activity} = activityStore;
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList
-          deleteActivity={deleteActivity}
-          submitting={submitting}
-          target={target}
-        />
+        <ActivityList/>
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && !editMode && (
-          <ActivityDetails/>
-        )}
-        {editMode && (
-          <ActivityForm
-            key={(selectedActivity && selectedActivity.id) || 0}
-          />
-        )}
+        <h2>Activity Filters</h2>
       </Grid.Column>
     </Grid>
   );
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+const HomePage = () => {
+    return (
+      <Container style={{marginTop: '7em'}}>
+          <h1>Welcome home page</h1>
+      </Container>
+    );
+};
+
+export default HomePage;
