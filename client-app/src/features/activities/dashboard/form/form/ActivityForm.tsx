43930d04cc6f0a04ca960f1dce18a4c489fdacf9
Redar <redar_chicho@hotmail.com>
2020-02-21 14:18:18 -0600

    coded

--- client-app/src/features/activities/dashboard/form/form/ActivityForm.tsx
+++ client-app/src/features/activities/dashboard/form/form/ActivityForm.tsx
@@ -2,7 +2,6 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import { ActivityFormValues } from '../../../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
-import ActivityStore from '../../../../../app/stores/activityStore'
 import { observer } from 'mobx-react-lite';
 import { RouteComponentProps } from 'react-router-dom';
 import { Form as FinalForm, Field } from 'react-final-form'
@@ -13,6 +12,7 @@ import { category } from '../options/CategoryOptions';
 import DateInput from '../../../../../app/common/form/DateInput';
 import { combineDateAndTime } from '../../../../../app/common/util/util';
 import { combineValidators, isRequired, composeValidators, hasLengthGreaterThan } from 'revalidate';
+import { RootStoreContext } from '../../../../../app/stores/rootStore';
 const validate = combineValidators({
     title: isRequired({ message: 'The event title is required' }),
     category: isRequired({ message: 'The event category is required' }),
@@ -29,9 +29,9 @@ interface DetailsParams {
     id: string
 }
 const ActivityForm: React.FC<RouteComponentProps<DetailsParams>> = ({ match, history }) => {
-    const activityStore = useContext(ActivityStore);
+    const rootStore = useContext(RootStoreContext);
     const { editActivity, createActivity, submitting,
-        loadActivity } = activityStore
+        loadActivity } = rootStore.activityStore
     const [activity, setActivity] = useState(new ActivityFormValues());
     const [loading, setLoading] = useState(false);
     useEffect(() => {
